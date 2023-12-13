535f0eb37d12f19a421a37befc0feaf9dcd08b82
utkarsh120794 <utkarshsin@gmail.com>
2023-12-12 17:27:44 -0800

    Updated the form

--- utkarsh_sinha/enroll/forms.py
+++ utkarsh_sinha/enroll/forms.py
@@ -6,4 +6,8 @@ class EmployeeRegistration(forms.ModelForm):
     class Meta:
         model = User
         fields = ['name', 'email', 'password']
-   
+        widgets = {
+        'name': forms.TextInput(attrs={'class': 'form-control'}),
+        'email' : forms.EmailInput (attrs={'class':'form-control'}),
+        'password': forms.PasswordInput(attrs={'class':'form-control'}),
+    }
