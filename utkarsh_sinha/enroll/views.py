8272458554f396941e448d591b4a8306387fcacb
utkarsh120794 <utkarshsin@gmail.com>
2023-12-12 17:18:33 -0800

    Created the form and the superuser

--- utkarsh_sinha/enroll/views.py
+++ utkarsh_sinha/enroll/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .forms import EmployeeRegistration
 
 # Create your views here.
 def add_show(request):
-    return render(request, 'addandshow.html')
+    if request.method == 'POST':
+        fm = EmployeeRegistration(request.POST)
+    else: 
+        fm =EmployeeRegistration()
+    return render(request, 'addandshow.html', {'form':fm})
+
